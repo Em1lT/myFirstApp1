ef421b53d4981a50a0f2d192eb140b37c02204bd
Emil T <37114731+Em1lT@users.noreply.github.com>
2019-01-21 10:42:02 +0200

    wbma

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,14 +10,25 @@ import { MediaProvider } from '../../providers/media/media';
 })
 export class HomePage {
 
+  pics: any;
   picArray: Pic[];
   url = "https://media.mw.metropolia.fi/wbma/uploads/";
-  constructor(public navCtrl: NavController,public http: HttpClient, private mediaProvider: MediaProvider) {
+  constructor(public navCtrl: NavController,public http: HttpClient, public mediaProvider: MediaProvider) {
 
   }
   ngOnInit(){
-    this.mediaProvider.getAllMedia();
+    this.getAllFile();
   }
+  getAllFile(){
+    this.mediaProvider.getAllMedia().subscribe((data: Pic[])=> {
+    console.log('data', data);
+    this.picArray = data.map((pic: Pic) => {
+      //add thumnail property to pic
+    return pic;
+    });
+  });
+  }
+  
   
   clicked(){
     alert("Clicked!!");
--- src/providers/media/media.ts
+++ src/providers/media/media.ts
@@ -9,21 +9,17 @@ import { Pic } from '../../interfaces/pic';
 */
 @Injectable()
 export class MediaProvider {
-
+  configUrl = 'https://media.mw.metropolia.fi/wbma';
   picArray: Pic[];
 
   constructor(public http: HttpClient) {
-    console.log('Hello MediaProvider Provider');
-  }
-  ngOnInit(){
-    this.getAllMedia()
+    
   }
   getAllMedia(){
-    return this.http.get<Pic[]>('https://media.mw.metropolia.fi/wbma/media')
-    .subscribe((data) =>{
-      this.picArray = data;
-      console.log("this",data);
-    });
+    return this.http.get<Pic[]>(this.configUrl + '/media');
   }
 }
-
+/*return this.getData()
+    .subscribe((data) =>{
+      this.picArray = data;
+*/
