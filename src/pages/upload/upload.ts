c69c0f5cfd12701d0e9e5c981f16f242f5864f33
Emil T <37114731+Em1lT@users.noreply.github.com>
2019-02-07 15:53:43 +0200

    there is now a image manipulation filters

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -19,18 +19,13 @@ export class UploadPage {
   filters = {
     brightness: 100,
     contrast: 100,
-    sepia: 100,
-    saturate: 100,
+    sepia: 0,
+    saturation: 100,
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public mediaprovider: MediaProvider,
     public loadingCtrl: LoadingController, private chooser: Chooser) {
   }
-  //not used in the chooser assignment
-  /*handleChange($event){
-    this.file = $event.target.files[0];
-    this.showPreview();
-  }*/
   showPreview(){
     const reader = new FileReader();
     reader.onloadend = () =>{
@@ -60,12 +55,16 @@ export class UploadPage {
       loading.dismiss();
     }, 2000);
   }
+  //This changes the colors in the image review section. It was done at the same time as the other assignment so 
+  //the image chooser is a little different.
   change(){
     var styles = {
-      filter: `brightness(${this.filters.brightness * 0.01})`
+      filter: `brightness(${this.filters.brightness * 0.01}) contrast(${this.filters.contrast * 0.01})
+      sepia(${this.filters.sepia * 0.01}) saturate(${this.filters.saturation * 0.01})`
       }
       return styles;
     }
+    //Chooser plugin was used to get the file.
     choose(){
       this.chooser.getFile("image/*")
       .then(file => {
@@ -77,5 +76,9 @@ export class UploadPage {
       })
       .catch((error: any) => console.error(error));
     }
-    
+     //not used in the chooser assignment
+  /*handleChange($event){
+    this.file = $event.target.files[0];
+    this.showPreview();
+  }*/ 
 }
